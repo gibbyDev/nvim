738795fbe3883f3176b09a84cde737ba778e9382
gibbyDev <118143161+gibbyDev@users.noreply.github.com>
2025-03-05 09:49:53 -0500

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -29,6 +29,61 @@ vim.keymap.set("n", "<leader>qq", ":q<CR>",
 -- Nvim Tree Toggle
 vim.api.nvim_set_keymap('n', '<leader>t', ':NvimTreeToggle<CR>', { noremap = true, silent = true }) 
 
+-- Move selected lines up and down in visual mode while preserving the selection
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Smoothly join lines without moving the cursor to the end of the joined line
+vim.keymap.set("n", "J", "mzJz")
+
+-- Search for the next occurrence and center it on the screen
+vim.keymap.set("n", "n", "nzzzv")
+
+-- Search for the previous occurrence and center it on the screen
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Re-indent a paragraph and move back to the original position after the edit
+vim.keymap.set("n", "=ap", "ma=ap'a")
+
+-- Restart the LSP (Language Server Protocol)
+vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
+
+-- The greatest remap ever: paste without affecting the clipboard or register
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Copy to system clipboard in normal and visual mode
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+
+-- Yank the entire line to the system clipboard
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Delete to void register, preventing overwriting of clipboard
+vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
+
+-- Open a new tmux window for session management with <C-f>
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- Format the current buffer using LSP on <leader>f
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- Navigate to the next error in the quickfix list, centering the cursor
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+
+-- Navigate to the previous error in the quickfix list, centering the cursor
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
+-- Navigate to the next item in the location list, centering the cursor
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+
+-- Navigate to the previous item in the location list, centering the cursor
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Search and replace the word under the cursor throughout the file, case-insensitive
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Make the current file executable with <leader>x
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
 vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
 
